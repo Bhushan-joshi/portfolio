db82332565b8a1454606dc31745ec833025062ff
Bhushan Joshi <bhushanjoshi911@gmail.com>
2022-07-08 23:01:36 +0530

    animated

--- src/components/projects/card.js
+++ src/components/projects/card.js
@@ -1,17 +1,40 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Transitions from "../Transitions";
 import Modal from "./modal";
+import { motion,AnimatePresence } from "framer-motion";
+import { useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 const Card = ({ item }) => {
   const [isOpen, setIsOpen] = useState(false);
-  console.log(isOpen);
   const closeModal = () => {
     setIsOpen(false);
   };
+  const [ref, inView] = useInView();
+  const animation = useAnimation();
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: .1,
+          type: "spring",
+          stiffness: 110,
+          bounce: 0.3,
+          delay:.2
+        },
+      });
+    }
+  }, [inView, animation]);
   return (
-    <>
-      <div
+    <Transitions>
+      <motion.div
+        animate={animation}
         onClick={() => setIsOpen(true)}
         className="max-w-xs rounded-lg overflow-hidden shadow-lg bg-white text-black cursor-pointer"
+        ref={ref}
+        style={{opacity:'0',y:'50px'}}
       >
         <img className="w-full" src={item.img} alt="Sunset in the mountains" />
         <div className="px-6 py-4">
@@ -23,17 +46,18 @@ const Card = ({ item }) => {
           {item.stack.map((st) => (
             <span
               key={st[0]}
-              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 sm:mx-4 mb-3 mt-0"
             >
               #{st}
             </span>
           ))}
         </div>
-      </div>
+      </motion.div>
       {isOpen && (
+        <AnimatePresence key={item.id}>
         <Modal setIsOpen={closeModal}>
           <div className=" w-full lg:max-w-full lg:flex rounded-lg">
-            <div class="sm:h-96 sm:w-96 flex-none bg-cover m-2 text-center overflow-hidden">
+            <div className="sm:h-96 sm:w-96 flex-none bg-cover m-2 text-center overflow-hidden">
               <img
                 className="w-full h-full rounded-md"
                 src={item.img}
@@ -45,15 +69,13 @@ const Card = ({ item }) => {
                 <div className="text-gray-900 font-bold sm:text-4xl text-2xl mb-2 mt-0 relative top-0">
                   {item.name}
                 </div>
-                <p className="text-gray-700 text-base">
-                  {item.description}
-                </p>
+                <p className="text-gray-700 text-base">{item.description}</p>
               </div>
               <div className="sm:px-6 sm:pt-4 pb-2">
                 {item.stack.map((st) => (
                   <span
                     key={st[0]}
-                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                    className="inline-block bg-gray-200 rounded-full  px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
                   >
                     #{st}
                   </span>
@@ -62,9 +84,20 @@ const Card = ({ item }) => {
             </div>
           </div>
         </Modal>
+        </AnimatePresence>
       )}
-    </>
+    </Transitions>
   );
 };
 
 export default Card;
+
+// // initial={{
+//   opacity:0,
+//   y:'50px',
+// }}
+// animate={{
+//   opacity:1,
+//   y:0,
+// }}
+// transition={{ duration: 1,type: "spring", stiffness: 110}}
--- src/components/projects/modal.js
+++ src/components/projects/modal.js
@@ -1,17 +1,51 @@
 import React from "react";
 import { CgClose } from "react-icons/cg";
+import { motion , AnimatePresence } from "framer-motion";
+
+const dropIn = {
+  hidden: {
+    y: "-100vh",
+    opacity: 0,
+  },
+  visible: {
+    y: "0",
+    opacity: 1,
+    transition: {
+      duration: 1,
+      type: "spring",
+      damping: 10,
+      stiffness: 110,
+    },
+  },
+  exit: {
+    y: "100vh",
+    opacity: 0,
+  },
+};
+
 const Modal = (props) => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-60 backdrop-blur-sm flex justify-center items-center ">
-      <div className=" relative bg-white w-11/12" style={{borderRadius:'6px'}}>
-        <CgClose
-          onClick={props.setIsOpen}
-          className="h-8 w-8 absolute right-4 top-2 text-slate-800 cursor-pointer"
-          title="close"
-        />
-        {props.children}
-      </div>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        variants={dropIn}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className="fixed inset-0 bg-black bg-opacity-60 backdrop-blur-sm flex justify-center items-center "
+      >
+        <div
+          className=" relative bg-white w-11/12"
+          style={{ borderRadius: "6px" }}
+        >
+          <CgClose
+            onClick={props.setIsOpen}
+            className="h-8 w-8 absolute right-4 top-2 text-slate-800 cursor-pointer"
+            title="close"
+          />
+          {props.children}
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
